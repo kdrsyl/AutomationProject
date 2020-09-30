45d829dd45fbc92d2bea0fde25525dc6db8f31a3
kdrsyl <kadersayligb@gmail.com>
2020-09-30 21:47:20 +0100

    I have a changes on java class

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        System.out.println("Hello World EU3");
+        // more changes are coming
+        // I am ready to commit this
+
     }
 }
